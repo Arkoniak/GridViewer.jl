381c3bb0abb0cd36a648a301d9b8e155c2e3e32a
Andrey Oskin <a_oskin@wargaming.net>
2020-01-07 13:49:19 +0200

    initial dev

---
+++ dev/search_index.js
@@ -0,0 +1,3 @@
+var documenterSearchIndex = {"docs":
+[{"location":"#GridViewer.jl-1","page":"Home","title":"GridViewer.jl","text":"","category":"section"},{"location":"#","page":"Home","title":"Home","text":"","category":"page"},{"location":"#","page":"Home","title":"Home","text":"Modules = [GridViewer]","category":"page"}]
+}
